c8afa76c3a9dda06a0f3e9960a7c7d543ceb4bb1
consol-ua <malygin91@gmail.com>
2021-02-23 18:52:42 +0200

    main

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { request, gql } from 'graphql-request'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <button>Test GraphQL</button>
+    </div>
+  );
+}
+
+export default App;
